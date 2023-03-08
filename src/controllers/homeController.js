3915f7c8a5a83e03af10274b9682964dabd98cf0
vancuong11 <cuongg121101@gmail.com>
2023-03-08 20:41:17 +0700

    Http Request Method & Database

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,19 @@
+import connection from '../configs/connectDB';
+
 const getHomePage = (req, res) => {
-    res.render('home', { title: 'Trang chu', day: 'Thứ Năm' });
+    let data = [];
+    connection.query('SELECT * FROM `users`', function (err, results, fields) {
+        results.map((row) => {
+            data.push({
+                id: row.id,
+                email: row.email,
+                address: row.address,
+                firstName: row.firstName,
+                lastName: row.lastName,
+            });
+        });
+        res.render('home', { dataUser: JSON.stringify(data) });
+    });
 };
 
 module.exports = {
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import configViewEngine from './configs/viewEngine';
 import initWebRoute from './router/web';
+
 require('dotenv').config();
 
 const app = express();
