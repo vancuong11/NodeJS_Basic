cd0a1942be783cc079033a18f9144175b22b4c91
vancuong11 <cuongg121101@gmail.com>
2023-03-08 21:19:55 +0700

    Loop Data EJS

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -3,16 +3,7 @@ import connection from '../configs/connectDB';
 const getHomePage = (req, res) => {
     let data = [];
     connection.query('SELECT * FROM `users`', function (err, results, fields) {
-        results.map((row) => {
-            data.push({
-                id: row.id,
-                email: row.email,
-                address: row.address,
-                firstName: row.firstName,
-                lastName: row.lastName,
-            });
-        });
-        res.render('home', { dataUser: JSON.stringify(data) });
+        res.render('home', { dataUser: results });
     });
 };
 
