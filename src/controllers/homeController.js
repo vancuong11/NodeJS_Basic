94b7047b51dabe33d26005c0c2a78e8f985b327f
vancuong11 <cuongg121101@gmail.com>
2023-03-12 11:20:46 +0700

    Update Single File

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,3 +1,4 @@
+import multer from 'multer';
 import connection from '../configs/connectDB';
 
 const getHomePage = (req, res) => {
@@ -64,6 +65,36 @@ const updateUser = (req, res) => {
     );
 };
 
+const uploadFile = (req, res) => {
+    res.render('uploadFile');
+};
+
+const upload = multer().single('profile_pic');
+
+let handleUploadProfilePic = async (req, res) => {
+    // 'profile_pic' is the name of our file input field in the HTML form
+
+    upload(req, res, function (err) {
+        // req.file contains information of uploaded file
+        // req.body contains information of text fields, if there were any
+
+        if (req.fileValidationError) {
+            return res.send(req.fileValidationError);
+        } else if (!req.file) {
+            return res.send('Please select an image to upload');
+        } else if (err instanceof multer.MulterError) {
+            return res.send(err);
+        } else if (err) {
+            return res.send(err);
+        }
+
+        // Display uploaded image for user validation
+        res.send(
+            `You have uploaded this image: <hr/><img src="/upload/${req.file.filename}" width="500"><hr /><a href="/upload">Upload another image</a>`,
+        );
+    });
+};
+
 module.exports = {
     getHomePage,
     getDetailPage,
@@ -71,4 +102,6 @@ module.exports = {
     deleteUser,
     editUser,
     updateUser,
+    uploadFile,
+    handleUploadProfilePic,
 };
--- src/router/web.js
+++ src/router/web.js
@@ -1,7 +1,30 @@
 import express from 'express';
 import homeController from '../controllers/homeController';
-
+import multer from 'multer';
 let router = express.Router();
+
+// handle file
+const storage = multer.diskStorage({
+    // dinh nghia noi luu tru
+    destination: function (req, file, cb) {
+        cb(null, './src/public/upload');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + '-' + file.originalname);
+    },
+});
+
+const imageFilter = function (req, file, cb) {
+    // Accept images only
+    if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+        req.fileValidationError = 'Only image files are allowed!';
+        return cb(new Error('Only image files are allowed!'), false);
+    }
+    cb(null, true);
+};
+
+// 'profile_pic' is the name of our file input field in the HTML form
+let upload = multer({ storage: storage, fileFilter: imageFilter });
 
 const initWebRoute = (app) => {
     // truyen bien cho view / Sử dụng lệnh If
@@ -19,6 +42,10 @@ const initWebRoute = (app) => {
     // edit user
     router.get('/edit/user/:id', homeController.editUser);
     router.post('/edit/user/:id', homeController.updateUser);
+
+    // upload file
+    router.get('/upload', homeController.uploadFile);
+    router.post('/upload-profile-pic', upload.single('profile_pic'), homeController.handleUploadProfilePic);
 
     return app.use('/', router);
 };
